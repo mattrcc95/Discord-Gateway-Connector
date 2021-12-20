50a34c4e5f8918ea1cbe5bf168d517b4f6d406b9
Matteo Ricci <mattrcc95@gmail.com>
2021-12-20 21:16:11 +0100

    removed typo

--- src/main/kotlin/com/gateway/comunication/indoor/guildrequest/RequestData.kt
+++ src/main/kotlin/com/gateway/comunication/indoor/guildrequest/RequestData.kt
@@ -3,9 +3,9 @@ package com.gateway.comunication.indoor.guildrequest
 import com.google.gson.annotations.SerializedName
 
 class RequestData(
-    @SerializedName("guild_id") val guildId: String = "902874779613282364",
- //   @SerializedName("guild_id") val guildId: String = "907344205653893230",
-//    @SerializedName("guild_id") val guildId: String = "879869697548505108",
+//  @SerializedName("guild_id") val guildId: String = "902874779613282364",
+    @SerializedName("guild_id") val guildId: String = "907344205653893230",
+//  @SerializedName("guild_id") val guildId: String = "879869697548505108",
     @SerializedName("query") val query: String? = "Animal",
     @SerializedName("limit") val limit: Int = 100,
     @SerializedName("presences") val presences: Boolean? = true,
--- src/main/kotlin/com/gateway/comunication/outdoor/member/GuildUser.kt
+++ src/main/kotlin/com/gateway/comunication/outdoor/member/GuildUser.kt
@@ -3,7 +3,7 @@ package com.gateway.comunication.outdoor.member
 import com.google.gson.annotations.SerializedName
 
 data class GuildUser(
-    @SerializedName("usernamer") val username: String,
+    @SerializedName("username") val username: String,
     @SerializedName("id") val userId: String,
     @SerializedName("discriminator") val discriminator: String,
     @SerializedName("avatar") val avatar: String,
